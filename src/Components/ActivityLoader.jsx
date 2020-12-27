2c9a3a4dc09f67d884d53de77e0f283892a12ce5
Owen Jones <owenthefirst93@gmail.com>
2020-12-27 13:48:43 -0500

    activityloader initialState and state hooks

--- src/Components/ActivityLoader.jsx
+++ src/Components/ActivityLoader.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Button} from '@material-ui/core'
 
-function ActivityLoader() {
+const  ActivityLoader = () => {
+    const initialState = {
+        activity : '',
+        accessibility: 0,
+        type: '',
+        participants: 1,
+        price: 0,
+        link: '',
+        key: ''
+    }
+
+    const [activity, setActivity] = useState(initialState)
+    const [submitted, setSubmitted] = useState(false)
+
+    const handleActivityChange = (event) => {
+        
+    }
     return (
-        <Button color='primary' variant='contained'>
+        <Button color='primary' variant='contained' size='large' >
             Try something new
         </Button>
         )
